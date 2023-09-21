a398dfaac5dca50d1f1a0d81cd9a5dd18682ff66
tamashiro-syuta <syuta5825@gmail.com>
2023-09-21 17:23:10 +0900

    ✨ 3000でhello world

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// index.ts
+import { serve } from 'bun';
+
+const server = serve({
+  port: 3000,
+  async fetch(req: Request) {
+    return new Response('hi mom');
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}...`);
