b8794738d67a497b47c0e8b1852251a0eb607556
tamashiro-syuta <syuta5825@gmail.com>
2023-09-21 18:33:15 +0900

    DB(ORM)の設定を追加

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+// drizzle.config.ts
+import type { Config } from 'drizzle-kit';
+
+export default {
+  schema: './src/**/schema.ts',
+  driver: 'pg',
+  out: './src/db/migrations',
+  verbose: true,
+} satisfies Config;
