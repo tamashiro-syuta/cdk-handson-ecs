21243ac143ea21996c8c61da27a34d2212aa6ca5
tamashiro-syuta <syuta5825@gmail.com>
2023-09-21 18:34:54 +0900

    clientsとindex.tsの変更

---
+++ src/db/client.ts
@@ -0,0 +1,42 @@
+// ./src/db/client.ts
+import { sleep } from 'bun';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import { Pool } from 'pg';
+
+export const pool = new Pool({
+  user: process.env.DB_USER ?? 'postgres',
+  password: process.env.DB_PASS ?? 'postgres',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT) ?? 5432,
+  database: process.env.DB_NAME ?? 'zon100',
+});
+
+export const db = drizzle(pool);
+
+pool.once('connect', async () => {
+  await migrate(db, { migrationsFolder: './src/db/migrations' });
+  console.log('First migration complete');
+});
+
+export const retryConnect = async (maxRetries = 10): Promise<void> => {
+  let isConnected = false;
+  let retries = 0;
+
+  while (!isConnected && retries < maxRetries) {
+    console.error('db connecting...');
+    await pool
+      .connect()
+      .then(() => {
+        isConnected = true;
+        retries = 0;
+        console.log('db connected successfully');
+      })
+      .catch(async () => {
+        console.error('db connection error. retry db connection...');
+        await sleep(2000);
+        isConnected = false;
+        retries++;
+      });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
-// index.ts
+// ./src/index.ts
 import { serve } from 'bun';
+import { eq } from 'drizzle-orm';
+import { db, retryConnect } from './db/client';
+import { todos } from './db/schema';
+
+await retryConnect();
 
 const server = serve({
   port: 3000,
   async fetch(req: Request) {
+    const url = new URL(req.url);
+
+    // Create a todo
+    if (url.pathname === '/todos' && req.method === 'POST') {
+      const body = await req.json();
+      const created = await db.insert(todos).values({ title: body.title }).returning();
+      return new Response(JSON.stringify(created));
+    }
+
+    // Update a todo
+    if (url.pathname === '/todos' && req.method === 'PUT') {
+      const body = await req.json();
+      if (!body.id) return new Response(JSON.stringify({ update: false }));
+      const updated = await db.update(todos).set(body).where(eq(todos.id, body.id)).returning();
+      return new Response(JSON.stringify(updated));
+    }
+
+    // Delete a todo
+    if (url.pathname === '/todos' && req.method === 'DELETE') {
+      const body = await req.json();
+      if (!body.id) return new Response(JSON.stringify({ delete: false }));
+      const deleted = await db.delete(todos).where(eq(todos.id, body.id)).returning();
+      return new Response(JSON.stringify(deleted));
+    }
+
+    // Get all todos
+    if (url.pathname === '/todos' && req.method === 'GET') {
+      const res = await db.select().from(todos);
+      return new Response(JSON.stringify(res));
+    }
+
     return new Response('hi mom');
   },
 });
